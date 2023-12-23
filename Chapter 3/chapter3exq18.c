c80624c2c28c2826b54100cb6f865542210d4858
Srish N <58882869+srish-cell@users.noreply.github.com>
2023-12-23 14:07:16 +0530

    Add files via upload

---
+++ Chapter 3/chapter3exq18.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main( )
+{
+int a = 500, b, c ;
+if ( a >= 400 )
+b = 300 ;
+c = 200 ;
+printf ( "%d %d\n", b, c ) ;
+int x = 10, y = 20 ;
+if ( x == y ) ;
+printf ( "%d %d\n", x, y ) ;
+return 0 ;
+}
