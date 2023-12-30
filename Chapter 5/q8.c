9927f3d366f48de06e2f5e9e7d34aad7355a342f
Srish N <58882869+srish-cell@users.noreply.github.com>
2023-12-30 10:23:18 +0530

    Update q8.c

---
+++ Chapter 5/q8.c
@@ -0,0 +1,13 @@
+/*
+Name: Srish N
+Roll No: CH.EN.U4CYS21081
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Octal equivalent of %d is %o\n",n,n);
+    return 0;
+}
